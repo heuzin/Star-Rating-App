01a93025d779ef0bc88cc83b321cd77fc83e8492
heuzin <matheusqs.arq@gmail.com>
2020-11-30 12:19:01 -0500

    setSelectedStars to select stars

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiStar } from 'react-icons/fi';
 
-const Star = ({ selected = false }) => (
-    <FiStar color={selected ? 'red' : 'grey'}/>
+const Star = ({ selected = false, onSelect}) => (
+    <FiStar color={selected ? 'red' : 'grey'} onClick={onSelect}/>
 )
 
 const createArray = ((length) => {
     return [...Array(length)]  
 })
 
-export default function StartRating({ totalStars = 10 }) {
-    return createArray(totalStars).map((n, i) => <Star key={i}/>)
+export default function StartRating({ totalStars = 5 }) {
+    const [selectedStarts, setSelectedStarts] = useState(0);
+    return (
+        <div>
+        {createArray(totalStars).map((n, i) => (
+            <Star key={i} selected={selectedStarts > i} onSelect={() => setSelectedStarts(i + 1)}/>
+            ))}
+        <p>{selectedStarts} of {totalStars}</p>
+        </div>
+    )
 }
--- src/components/StartRatingApp.js
+++ src/components/StartRatingApp.js
@@ -4,7 +4,6 @@ import StartRating from './StarRating';
 function App() {
   return (
     <div className="App">
-      <p>App</p>
       <StartRating />
     </div>
   );
