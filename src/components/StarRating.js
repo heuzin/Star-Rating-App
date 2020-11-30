5c3843874f787baea87fd1ab6baf09673995af77
heuzin <matheusqs.arq@gmail.com>
2020-11-30 11:20:05 -0500

    creat array of stars

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FiStar } from 'react-icons/fi';
 
-export default function StartRating() {
-    return [
-        <FiStar color='red'/>,
-        <FiStar color='red'/>,
-        <FiStar color='red'/>,
-        <FiStar color='green'/>,
-        <FiStar color='green'/>
-    ]
+const Star = ({ selected = false }) => (
+    <FiStar color={selected ? 'red' : 'grey'}/>
+)
+
+const createArray = ((length) => {
+    return [...Array(length)]  
+})
+
+export default function StartRating({ totalStars = 10 }) {
+    return createArray(totalStars).map((n, i) => <Star key={i}/>)
 }
